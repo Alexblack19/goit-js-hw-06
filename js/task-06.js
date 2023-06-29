90164a4eb4b9801d8592d220635bfe461f9240e6
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-06-30 00:00:50 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,24 +2,24 @@ const inputEl = document.querySelector("#validation-input");
 
 inputEl.addEventListener("input", (event) => {
   const addTextLength = event.currentTarget.value.length;
-  return addTextLength;
+  console.log(addTextLength);
+
+
+  const onValidationInput = () => {
+    if (Number(inputEl.dataset.length) === addTextLength) {
+      console.log("Hello");
+    //   inputEl.classList.add("valid");
+      return;
+    }
+    console.log("No");
+    //   inputEl.classList.add("invalid");
+    return
+  };
+  
+  inputEl.addEventListener("blur", onValidationInput);
+
+
+
 });
-console.log(addTextLength);
 
 
-
-
-
-
-
-const onValidationInput = () => {
-  if (Number(inputEl.dataset.length) === i) {
-    console.log("Hello");
-    // inputEl.classList.add("valid");
-    return;
-  }
-  console.log("No");
-  //   inputEl.classList.add("invalid");
-};
-
-inputEl.addEventListener("blur", onValidationInput);
