631c7120817d392804c156eb181e5a6773b1e2d8
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-06-30 22:51:45 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -8,11 +8,18 @@ function getRandomHexColor() {
 const inputEl = document.querySelector('[type="number"]')
 console.log(inputEl.value);
 
+if(Number(inputEl.value) === 5) {
+  console.log('Hello');
+}
+
+
 
 const btnCreateEl = document.querySelector('button[data-create]');
 console.log(btnCreateEl);
+
 const btnDestroyEl = document.querySelector('button[data-destroy]');
 console.log(btnDestroyEl);
+
 const divboxes = document.querySelector("#boxes");
 console.log(divboxes);
 
