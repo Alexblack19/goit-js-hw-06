97b44d4dc28fe137950436ef132d8d8f22c5608f
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-06-30 12:43:59 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,22 +1,22 @@
 //============
 //* Варіант 1
 //============
-// const inputEl = document.querySelector("#validation-input");
+const inputEl = document.querySelector("#validation-input");
 
-// inputEl.addEventListener("blur", onInputBlur);
+inputEl.addEventListener("blur", onInputBlur);
 
-// function onInputBlur() {
-//   const addTextLength = inputEl.value.trim().length;
-//   const dataTextLength = Number(inputEl.getAttribute("data-length"));
+function onInputBlur() {
+  const addTextLength = inputEl.value.trim().length;
+  const dataTextLength = Number(inputEl.getAttribute("data-length"));
 
-//   if (addTextLength === dataTextLength) {
-//     inputEl.classList.remove("invalid");
-//     inputEl.classList.add("valid");
-//   } else {
-//     inputEl.classList.remove("valid");
-//     inputEl.classList.add("invalid");
-//   }
-// }
+  if (addTextLength === dataTextLength) {
+    inputEl.classList.remove("invalid");
+    inputEl.classList.add("valid");
+  } else {
+    inputEl.classList.remove("valid");
+    inputEl.classList.add("invalid");
+  }
+}
 
 //============
 //* Варіант 2
@@ -26,7 +26,7 @@
 // inputEl.addEventListener("blur", onInputBlur);
 
 // function onInputBlur(event) {
-//   const addTextLength = event.target.value.trim().length;
+//   const addTextLength = event.currentTarget.value.trim().length;
 //   const dataTextLength = Number(inputEl.getAttribute("data-length"));
 
 //   if (addTextLength === dataTextLength) {
@@ -37,25 +37,3 @@
 //     inputEl.classList.add("invalid");
 //   }
 // }
-
-//============
-//* Варіант 3
-//============
-const inputEl = document.querySelector("#validation-input");
-
-inputEl.addEventListener("blur", onInputBlur);
-const dataTextLength = Number(inputEl.getAttribute("data-length"));
-
-function onInputBlur(event) {
-  inputEl.addEventListener("input", () => {
-    const addTextLength = event.target.value.trim().length;
-
-    if (addTextLength === dataTextLength) {
-      inputEl.classList.remove("invalid");
-      inputEl.classList.add("valid");
-    } else {
-      inputEl.classList.remove("valid");
-      inputEl.classList.add("invalid");
-    }
-  });
-}
