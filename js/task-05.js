f53d94087a16274e64f13d06e420f45f2027ceba
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-06-30 16:27:16 +0300

    update

--- js/task-05.js
+++ js/task-05.js
@@ -2,6 +2,5 @@ const textInput = document.querySelector("#name-input");
 const textOutput = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (event) => {  
-  textOutput.textContent = event.currentTarget.value;
-  console.dir(event.currentTarget);
+  textOutput.textContent = event.currentTarget.value.trim();  
 });
