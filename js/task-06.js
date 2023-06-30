6587745a1c7fc9474c06c192140e775cc101576d
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-06-30 10:25:39 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,11 @@
+//============
+//* Варіант 1
+//============
 const inputEl = document.querySelector("#validation-input");
 
 const onValidationInput = () => {
-  const addTextLength = inputEl.value.trim().length;
-  const dataTextLength = Number(inputEl.getAttribute("data-length"));
+  const addTextLength = inputEl.value.trim().length;  
+  const dataTextLength = Number(inputEl.getAttribute("data-length")); 
   
   if (addTextLength === dataTextLength) {
     inputEl.classList.remove("invalid");
@@ -14,3 +17,23 @@ const onValidationInput = () => {
 };
 
 inputEl.addEventListener("blur", onValidationInput);
+
+//============
+//* Варіант 2
+//============
+// const inputEl = document.querySelector("#validation-input");
+
+// const onValidationInput = (event) => {
+//   const addTextLength = event.target.value.trim().length;
+//   const dataTextLength = Number(inputEl.getAttribute("data-length"));
+  
+//   if (addTextLength === dataTextLength) {
+//     inputEl.classList.remove("invalid");
+//     inputEl.classList.add("valid");
+//   } else {
+//     inputEl.classList.remove("valid");
+//     inputEl.classList.add("invalid");
+//   }
+// };
+
+// inputEl.addEventListener("blur", onValidationInput);
