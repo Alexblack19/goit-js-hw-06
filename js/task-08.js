5f949d2e2bfa1be7e1d16a2aa7e319543cad909a
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-06-30 18:45:17 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,5 +1,18 @@
-const formEl = document.querySelector('login-form')
-console.dir(formEl);
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onFormSubmit);
 
-
+function onFormSubmit(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+  const data = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(data);
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
