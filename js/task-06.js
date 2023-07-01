7e506ef88973188ca28ad5c84d30c32570706811
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-01 14:27:10 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -9,17 +9,37 @@ function onInputBlur() {
   const addTextLength = inputEl.value.trim().length;
   const dataTextLength = Number(inputEl.getAttribute("data-length"));
 
-  if (addTextLength === dataTextLength) {
-    inputEl.classList.remove("invalid");
-    inputEl.classList.add("valid");
-  } else {
-    inputEl.classList.remove("valid");
-    inputEl.classList.add("invalid");
+  addTextLength === dataTextLength
+    ? changeClass("valid", "invalid")
+    : changeClass("invalid", "valid");
+
+  function changeClass(first, second) {
+    inputEl.classList.remove(second);
+    inputEl.classList.add(first);
   }
 }
-
 //============
 //* Варіант 2
+//============
+// const inputEl = document.querySelector("#validation-input");
+
+// inputEl.addEventListener("blur", onInputBlur);
+
+// function onInputBlur() {
+//   const addTextLength = inputEl.value.trim().length;
+//   const dataTextLength = Number(inputEl.getAttribute("data-length"));
+
+//   if (addTextLength === dataTextLength) {
+//     inputEl.classList.remove("invalid");
+//     inputEl.classList.add("valid");
+//   } else {
+//     inputEl.classList.remove("valid");
+//     inputEl.classList.add("invalid");
+//   }
+// }
+
+//============
+//* Варіант 3
 //============
 // const inputEl = document.querySelector("#validation-input");
 
