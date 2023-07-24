98d20bfcac066c05bfae98e390b0f26626a91aca
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-24 16:48:34 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -5,5 +5,5 @@ categoriesNumber.forEach(categories => {
     const categoriesName = categories.firstElementChild;
     console.log(`Category: ${categoriesName.textContent}`);
     const elementsNumber = categories.lastElementChild.children;
-    console.log(`Elements: ${elementsNumber.length}`);   
+    console.log(`Elements: ${elementsNumber.length}`);
 });
